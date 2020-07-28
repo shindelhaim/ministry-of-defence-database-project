50c27320687cb0a9affbbe5c8cb404a4cf7fecd6
shindelhaim <sz@sem-wolf.org>
2020-07-29 02:51:09 +0300

    Add the indexing feature and changes because of it

--- src/db.py
+++ src/db.py
@@ -22,6 +22,7 @@ class DBTable(db_api.DBTable):
         self.fields = fields
         self.key_field_name = key_field_name
         self.path_file = os.path.join('db_files', self.name + '.db')
+        self.indexes = []
 
         # create shelve file
         s = shelve.open(self.path_file)
@@ -33,10 +34,73 @@ class DBTable(db_api.DBTable):
         return [field.name for field in self.fields]
 
 
+    def get_path_index_file(self, field_name):
+        return os.path.join('db_files', 'index_' + field_name + '_' + self.name + '.db')
+
+
     def count(self) -> int:
         return self.num_record
 
 
+    def update_records_in_every_indexes(self, old_record, new_record):
+        if old_record != [] or old_record[self.key_field_name] != new_record[self.key_field_name]:
+            raise ValueError("The key field couldn't be updated")
+
+        for field_name in new_record.keys():
+            if field_name not in self.indexes:
+                continue
+
+            if field_name not in old_record.keys():
+                index_file = shelve.open(self.get_path_index_file(field_name), writeback=True)
+
+                if str(new_record[field_name]) in index_file.keys():
+                    index_file[str(new_record[field_name])] += [new_record[self.key_field_name]]
+
+                else:
+                    index_file[str(new_record[field_name])] = [new_record[self.key_field_name]]
+
+                index_file.close()
+
+            else:
+                if new_record[field_name] == old_record[field_name]:
+                    continue
+                
+                index_file = shelve.open(self.get_path_index_file(field_name), writeback=True)
+                index_file[str(old_record[field_name])].remove(old_record[self.key_field_name])
+
+                if index_file[str(old_record[field_name])] == []:
+                    index_file.pop(str(old_record[field_name]))
+
+                if str(new_record[field_name]) in index_file.keys():
+                    index_file[str(new_record[field_name])] += [new_record[self.key_field_name]]
+
+                else:
+                    index_file[str(new_record[field_name])] = [new_record[self.key_field_name]]
+
+                index_file.close()
+
+
+    def delete_records_from_every_indexes(self, records: List[Dict[str, Any]]):
+
+        for index in self.indexes:
+            is_index_file_open = False
+            
+            for record in records:
+                if index in record.keys():
+                    
+                    if not is_index_file_open:
+                        index_file = shelve.open(self.get_path_index_file(index), writeback=True)
+                        is_index_file_open = True
+                    
+                    index_file[str(record[index])].remove(record[str(self.key_field_name)])
+                    
+                    if index_file[str(record[index])] == []:
+                        index_file.pop(str(record[index]))
+            
+            if is_index_file_open:
+                index_file.close()
+
+    
     def insert_record(self, values: Dict[str, Any]) -> None:
         if self.key_field_name not in values.keys():
             raise ValueError("The key is missing")
@@ -47,8 +111,9 @@ class DBTable(db_api.DBTable):
             s.close()
             raise ValueError("The key must be unique")
  
-        self.fields += [ DBField(item, Any) for item in values.keys() if item not in self.get_names_fields()]       
+        self.fields += [ DBField(item, Any) for item in values.keys() if item not in self.get_names_fields()]    
         s[str(values[self.key_field_name])] = values
+        self.update_records_in_every_indexes({}, values)   
         self.num_record += 1
         s.close()
         
@@ -58,8 +123,9 @@ class DBTable(db_api.DBTable):
 
         if str(key) not in s.keys():
             s.close()
-            raise ValueError("The key isn't exists")
- 
+            raise ValueError("The key doesn't exist")
+        
+        self.delete_records_from_every_indexes([s[str(key)]])
         s.pop(str(key))
         self.num_record -= 1
         s.close()
@@ -79,30 +145,63 @@ class DBTable(db_api.DBTable):
                 
                 if not is_criteria_met:
                     return False
+
+            else:
+                return False
         
         return True
 
 
     def delete_records(self, criteria: List[SelectionCriteria]) -> None:
-        s = shelve.open(self.path_file, writeback=True)
-        
+        table_file = shelve.open(self.path_file, writeback=True)
+        list_match_records = []
+
         # using hash index by key
         for item in criteria:
             if item.field_name == self.key_field_name and item.operator == '=':
-                record = s[str(item.value)]
+                record = table_file.get(str(item.value), None)
 
-                if self.are_criterias_met(record, criteria):
-                    s.pop(str(item.value))
-                    self.num_record -= 1
-                    s.close()
+                if record is None:
                     return
 
-        for record in s.values():
+                if self.are_criterias_met(record, criteria):
+                    table_file.pop(str(item.value))
+                    self.delete_records_from_every_indexes([record])
+                    self.num_record -= 1
+                    table_file.close()
+                
+                return
+
+        # using hash index if exist
+        for item in criteria:
+            if item.field_name in self.indexes and item.operator == '=':
+                index_file = shelve.open(self.get_path_index_file(item.field_name), 'r')
+                records_keys = table_file.get(str(item.value), None)
+
+                if records_keys is None:
+                    return
+                
+                for key in records_keys:
+                    record = table_file[str(key)]
+
+                    if self.are_criterias_met(record, criteria):
+                        table_file.pop(str(key))
+                        list_match_records += [record]
+                        self.num_record -= 1
+
+                table_file.close()
+                index_file.close()
+                self.delete_records_from_every_indexes(list_match_records)
+                return
+    
+        for record in table_file.values():
             if self.are_criterias_met(record, criteria):
-                s.pop(str(record[self.key_field_name]))
+                table_file.pop(str(record[self.key_field_name]))
+                list_match_records += [record]
                 self.num_record -= 1
         
-        s.close()
+        table_file.close()
+        self.delete_records_from_every_indexes(list_match_records)
 
 
     def get_record(self, key: Any) -> Dict[str, Any]:
@@ -117,38 +216,102 @@ class DBTable(db_api.DBTable):
         
         if str(key) not in s.keys():
             s.close()
-            raise ValueError("The key isn't exists")
+            raise ValueError("The key doesn't exist")
         
         self.fields += [ DBField(item, Any) for item in values.keys() if item not in self.get_names_fields()]
-        s[str(key)].update(values)
+        old_record = s[str(key)]
+        new_record = s[str(key)].update(values)
+        self.update_records_in_every_indexes(old_record, new_record)
         s.close()
 
 
     def query_table(self, criteria: List[SelectionCriteria]) -> List[Dict[str, Any]]:
-        s = shelve.open(self.path_file)
-
+        table_file = shelve.open(self.path_file)
         list_match_records = []
         
         # using hash index by key
         for item in criteria:
-            if item.field_name == self.key_field_name and item.operator == '==':
-                record = s[str(item.value)]
+            if item.field_name == self.key_field_name and item.operator == '=':
+                record = table_file.get(str(item.value), None)
+
+                if record is None:
+                    return []
 
                 if self.are_criterias_met(record, criteria):
-                    s.close()
+                    table_file.close()
                     return [record]
+                
+                return []
+        
+        # using hash index if exist
+        for item in criteria:
+            if item.field_name in self.indexes and item.operator == '=':
+                index_file = shelve.open(self.get_path_index_file(item.field_name))
+                id_records = index_file.get(str(item.value), None)
 
-        for record in s.values():
+                if id_records is None:
+                    return []
+                
+                for id_record in id_records:
+                    record = table_file[str(id_record)]
+                    
+                    if self.are_criterias_met(record, criteria):
+                        list_match_records += [record]
+
+                table_file.close()
+                index_file.close()
+                
+                return list_match_records
+
+        for record in table_file.values():
             if self.are_criterias_met(record, criteria):
                 list_match_records += [record]
         
-        s.close()
+        table_file.close()
         return list_match_records
 
 
-###############
     def create_index(self, field_to_index: str) -> None:
-        raise NotImplementedError
+        if field_to_index not in self.get_names_fields():
+            raise ValueError("Field index doesn't exist in table's fields")
+
+        if field_to_index in self.indexes or field_to_index == self.key_field_name:
+            return
+
+        index_file = shelve.open(self.get_path_index_file(field_to_index), writeback=True)
+        table_file = shelve.open(self.path_file)
+
+        for record in table_file.values():
+            key_index = record.get(field_to_index, None)
+
+            if key_index is None:
+                continue
+
+            key_index = str(key_index)
+            
+            if key_index in index_file.keys():
+                index_file[key_index] += [record[self.key_field_name]]
+
+            else:
+                index_file[key_index] = [record[self.key_field_name]]
+
+        table_file.close()
+        index_file.close()
+
+        # update the file database.csv
+        with open('database.csv','r') as csv_file:
+            csv_reader = csv.reader(csv_file)
+            lines = []
+            for row in csv_reader:
+                if self.name == row[0]:
+                    row[3] += [field_to_index]
+
+                lines += [row]
+            
+        with open('database.csv','w',newline='') as csv_file:
+            csv_writer = csv.writer(csv_file) 
+            for line in lines:
+                csv_writer.writerow(line)
 
 
 class DataBase(db_api.DataBase):
@@ -156,6 +319,10 @@ class DataBase(db_api.DataBase):
         self.db_tables = {}
         self.num_tables_in_DB = 0
         self.reload_from_disk()
+
+
+    def get_db_field_obj(self, data_field: List):
+        return DBField(data_field[0], eval(data_field[1]))
         
 
     def reload_from_disk(self):
@@ -164,8 +331,15 @@ class DataBase(db_api.DataBase):
 
             for row in csv_reader:
                 table_name = row[0]
-                self.db_tables[table_name] = DBTable(table_name, row[1], row[2])
+                list_fields = list(map(self.get_db_field_obj, eval(row[1])))
+                key_field_name = row[2]
+                self.db_tables[table_name] = DBTable(table_name, list_fields, key_field_name)
+                self.db_tables[table_name].indexes = eval(row[3])
                 self.num_tables_in_DB += 1
+
+
+    def get_data_field(self, field: DBField):
+        return [field.name, str(field.type)]
 
 
     def create_table(self, table_name: str, fields: List[DBField], key_field_name: str) -> DBTable:
@@ -180,7 +354,8 @@ class DataBase(db_api.DataBase):
 
         with open('database.csv', "a", newline='') as csv_file:
             csv_writer = csv.writer(csv_file)
-            data_table = [table_name, fields, key_field_name]
+            list_data_fields = list(map(self.get_data_field, fields))
+            data_table = [table_name, list_data_fields, key_field_name, []]
             csv_writer.writerow(data_table)
 
         return self.db_tables[table_name]
